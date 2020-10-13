96ae49aea23a4f060ca67ee8455f5a0e285e5f71
tomotsune <tomotsune@foxmail.com>
2020-10-13 19:33:44 +0800

    non-head forward list

--- link.h
+++ link.h
@@ -23,28 +23,53 @@ struct Node {
 template<typename ElemType>
 class Link {
 private:
-    Node<ElemType> *first;
     Node<ElemType> *head;
-
-    int length{};
 public:
 
     Link();
 
+    virtual ~Link();
+
     Node<ElemType> *getElem(int pos);
 
-    void insert(int pos, ElemType val);
+    void insert(int pos, const ElemType &val);
+
+    void remove(int pos);
+
+    void insertNextNode(Node<ElemType> *n, const ElemType &val);
+
+    Node<ElemType> *LocateElem(const ElemType &val) const;
+
+
+    [[nodiscard]] int length() const;
 
     friend ostream &operator<<(ostream &os, const Link &link) {
         auto temp{link.head->next};
         while (nullptr != temp) {
-            os << temp->val<<" ";
-            temp=temp->next;
+            os << temp->val << " ";
+            temp = temp->next;
         }
         return os;
     }
 };
 
+//constructs.
+template<typename ElemType>
+Link<ElemType>::Link():head(new Node<ElemType>{}) {
+    head->next = nullptr;
+}
+
+//destruct.
+template<typename ElemType>
+Link<ElemType>::~Link() {
+    while (head != nullptr) {
+        auto temp{head};
+        head = head->next;
+        delete temp;
+    }
+}
+
+//Find element in bit order
 template<typename ElemType>
 Node<ElemType> *Link<ElemType>::getElem(int pos) {
     if (pos < 0)
@@ -60,22 +85,66 @@ Node<ElemType> *Link<ElemType>::getElem(int pos) {
     return temp;
 }
 
+//Post-insertion operation of specified node
 template<typename ElemType>
-void Link<ElemType>::insert(int pos, ElemType val) {
+void Link<ElemType>::insertNextNode(Node<ElemType> *n, const ElemType &val) {
+    if (n == nullptr)
+        throw "error: insertNextNode(Node, val): null pointer!";
+    auto node{new Node<ElemType>{val}};
+    node->next = n->next;
+    n->next = node;
+}
+
+//Insert an element in bit order
+template<typename ElemType>
+void Link<ElemType>::insert(int pos, const ElemType &val) {
     ++pos; //根据获取的是位序还是下表决定是否添加此步骤.
-    if (pos < 1)
-        throw "the pos is out of range";
     try {
+        if (pos < 1)
+            throw "error: inser(int, ElmeType): underflow";
         auto temp{getElem(pos - 1)};
-        auto node{new Node<ElemType>(val)};
-        node->next = temp->next;
-        temp->next = node;
+        insertNextNode(temp, val);
     } catch (const char *msg) {
         cerr << msg << endl;
     }
 }
 
 template<typename ElemType>
-Link<ElemType>::Link():head(new Node<ElemType>{}) {
-    head->next= nullptr;
+void Link<ElemType>::remove(int pos) {
+    ++pos;
+    try {
+        if (pos < 1)
+            throw "remove(int pos): underflow";
+        auto temp{getElem(pos - 1)};
+        if(temp== nullptr||temp->next== nullptr)
+            throw "error: remove(int pos): overflow";
+        auto removeNode{temp->next};
+        temp->next=removeNode->next;
+        delete removeNode;
+    } catch (const char *msg) {
+        cerr << msg << endl;
+    }
 }
+
+//Find by value
+template<typename ElemType>
+Node<ElemType> *Link<ElemType>::LocateElem(const ElemType &val) const {
+    auto temp{head->next};
+    while (temp != nullptr && temp->val != val)
+        temp = temp->next;
+    return temp;
+}
+
+template<typename ElemType>
+int Link<ElemType>::length() const {
+    int len{};
+    auto temp{head};
+    while (temp->next != nullptr) {
+        temp = temp - next;
+        ++len;
+    }
+    return len;
+}
+
+
+
