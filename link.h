bf60a770cd04a93e149d404de5a7646a54fac98e
tomotsune <tomotsune@foxmail.com>
2020-10-13 16:54:22 +0800

    has no head pointer

--- link.h
+++ link.h
@@ -1,245 +1,81 @@
 #pragma once
+
 #include<iostream>
+
 using namespace std;
-template<typename T>
-struct Node
-{
-	T val;
-	Node* next;
 
-    Node()=default;
+template<typename ElemType>
+struct Node {
+    ElemType val;
+    Node *next;
 
-    explicit Node(T x) :val(x), next(nullptr) {}
+    Node() = default;
 
-    bool operator<(const Node &rhs) const {
-        return val<rhs.val;
-    }
+    explicit Node(ElemType x) : val(x), next(nullptr) {}
 
-    Node(const Node &rhs) {
-        val = rhs.val;
-    }
-    void operator=(const Node &rhs) {
-        val = rhs.val;
-    }
+    Node(const Node &rhs) { val = rhs.val; }
+
+    bool operator<(const Node &rhs) const { return val < rhs.val; }
+
+    void operator=(const Node &rhs) { val = rhs.val; }
 };
-template<typename T>
-class Link
-{
+
+template<typename ElemType>
+class Link {
 private:
-	Node<T>* first;
-	Node<T>* head;
+    Node<ElemType> *first;
+    Node<ElemType> *head;
 
-	int length{};
+    int length{};
 public:
-	Link();
-	Link(const Link& l);
-	void insertHead(T var);
-	void insert(T val, const int& pos);
-	void addByOrdered(const T& val);
-	void remove(const T& val);
-	int getLength();
-	void reverse();
-	int find(T val);
-	void print();
-	void sort();
-	void connect(const Link& l);
 
-    Node<T> *getHead() const;
+    Link();
 
-    ~Link();
+    Node<ElemType> *getElem(int pos);
+
+    void insert(int pos, ElemType val);
+
+    friend ostream &operator<<(ostream &os, const Link &link) {
+        auto temp{link.head->next};
+        while (nullptr != temp) {
+            os << temp->val<<" ";
+            temp=temp->next;
+        }
+        return os;
+    }
 };
 
-template<typename T>
-inline Link<T>::Link()
-{
-	head =new Node<T>{};
-	first=head->next;
-	length == 0;
-}
-
-template<typename T>
-inline void Link<T>::insertHead(T var)
-{
-	insert(var, 0);
-}
-
-template<typename T>
-inline void Link<T>::insert(T val, const int& pos)
-{
-	if (pos < 0) {
-		cout << "pos must be greater than zero" << endl;
-		return;
-	}
-	int index{ 1 };//walking stick of temp
-	Node<T>* temp{ first };
-	auto* node = new Node<T>{ val };
-	if (pos == 0) {
-		node->next = temp;
-		first = node;
-		length++;
-		return;
-	}
-	//将temp置于要插入的节点之前.
-	while (temp != nullptr && index < pos) {
-		temp = temp->next;
-		++index;
-	}
-	//插入位置不当
-	if (temp == nullptr) {
-		cout << "Insert failed!" << endl;
-	}
-	//交接后续结点.
-	node->next = temp->next;
-	temp->next = node;
-	++length;
-}
-
-template<typename T>
-inline void Link<T>::remove(const T& val)
-{
-	int pos = find(val);
-	if (-1 == pos) {
-		cout << "delete failed";
-		return;
-	}
-	if (0==pos) {
-		first = first->next;
-		--length;
-		return;
-	}
-	int index{ 0 };
-	Node<T>* temp = first;
-	while (++index < pos) {
-		temp = temp->next;
-	}
-	//temp停在了将要删除的element之前.
-	//为了防止temp->next->next空指针异常.
-	Node<T>* removeNode{ temp->next };
-
-	temp->next = temp->next->next;
-
-	delete removeNode;
-	--length;
-}
-
-template<typename T>
-inline int Link<T>::getLength()
-{
-	return length;
-}
-
-template<typename T>
-inline void Link<T>::reverse()
-{
-	if (first == nullptr) {
-		return;
-	}
-	Node<T>* curNode{ first }, * nextNode{ first->next }, * temp;
-	//保存3号位置结点,
-	// 把1号位置结点放到3号职位,
-	// 2号移到1号,
-	// 将被保存的原三号位置结点放到2号位置.
-	while (nextNode!=nullptr)
-	{
-		temp = nextNode->next;
-		nextNode->next = curNode;
-		curNode = nextNode;
-		nextNode = temp;
-	}
-	first->next = nullptr ;
-	first = curNode;
-}
-
-template<typename T>
-inline int Link<T>::find(T val)
-{
-	Node<T>* temp = first;
-	int index{};
-	while (temp != nullptr) {
-		if (temp->val == val) {
-			return index;
-		}
-		temp = temp->next;
-		++index;
-	}
-	return -1;
-}
-
-template<typename T>
-inline void Link<T>::print()
-{
-	if (first == nullptr) {
-		cout << "List is empty" << endl;
-		return;
-	}
-	Node<T>* temp{ first };
-	while (temp!=nullptr)
-	{
-		cout << temp->val << " ";
-		temp = temp->next;
-	}
-	cout << endl;
-}
-
-template<typename T>
-inline Link<T>::~Link()
-{
-	Node<T>* temp;
-	for (int  i = 0; i < length+1; i++)
-	{
-		temp = head;
-		head = head->next;
-		delete temp;
-	}
-}
-
-template<typename T>
-void Link<T>::sort() {
-    if (length == 1)return;
-    for (auto p = first; p->next != nullptr; p = p->next) {
-        for (auto q = p->next; q != nullptr; q = q->next) {
-            if (*p < *q) {
-                auto temp{*q};
-                *q = *p;
-                *p = temp;
-            }
-        }
-    }
-}
-
-template<typename T>
-void Link<T>::connect(const Link& l) {
-    if(first== nullptr)return;
-    auto temp{first};
-    while(temp->next!= nullptr){
-        temp=temp->next;
-    }
-    temp->next=l.first;
-}
-
-template<typename T>
-void Link<T>::addByOrdered(const T& val) {
-    auto temp{first};
-    auto index{0};
-    while(temp!= nullptr){
-        if(temp->val >= val)break;
-        temp=temp->next;
+template<typename ElemType>
+Node<ElemType> *Link<ElemType>::getElem(int pos) {
+    if (pos < 0)
+        throw "the pos is out of range";
+    auto temp{head};
+    int index{0};
+    while (temp != nullptr && index < pos) {
+        temp = temp->next;
         ++index;
     }
-    insert(val,index);
+    if (temp == nullptr)
+        throw "the pos is out of range";
+    return temp;
 }
 
-template<typename T>
-Link<T>::Link(const Link &l):Link() {
-    auto temp{l.head};
-    for (int i = 0; i < length; ++i) {
-        insertHead(temp->val);
-        temp=temp->next;
+template<typename ElemType>
+void Link<ElemType>::insert(int pos, ElemType val) {
+    ++pos; //根据获取的是位序还是下表决定是否添加此步骤.
+    if (pos < 1)
+        throw "the pos is out of range";
+    try {
+        auto temp{getElem(pos - 1)};
+        auto node{new Node<ElemType>(val)};
+        node->next = temp->next;
+        temp->next = node;
+    } catch (const char *msg) {
+        cerr << msg << endl;
     }
 }
 
-template<typename T>
-Node<T> *Link<T>::getHead() const {
-    return head;
+template<typename ElemType>
+Link<ElemType>::Link():head(new Node<ElemType>{}) {
+    head->next= nullptr;
 }
-
--- main.cpp
+++ main.cpp
@@ -4,111 +4,13 @@
 #include <iostream>
 #include"link.h"
 using namespace std;
-struct Empoyee {
-    string name;
-    int id;
-    friend std::ostream& operator<<(std::ostream& os, const Empoyee& e) {
-        os << "name: " << e.name<<","<<"id: "<<e.id;
-        return os;
-    };
-    bool operator==(const Empoyee& e) {
-        return (name == e.name && id == e.id);
-    }
-};
 int main(int argc, char* argv[])
 {
     Link<int> list;
-    list.insertHead(6);
-    list.insertHead(4);
-    list.insertHead(3);
-    list.insertHead(2);
-    list.insertHead(1);
+    list.insert(0,1);
+    list.insert(1,2);
+    list.insert(2,3);
 
-    //1. test LinkPrint()
-    list.print();
-
-    //2. 获得头指针.
-    cout<<list.getHead()<<endl;
-
-    //3. 向pos=4的位置插入结点为5的值.
-    list.insert(5,4);
-    list.print();
-
-    //4. 删除链表中第一个值为6的结点.
-    list.remove(6);
-    list.print();
-
-    //5. 链接另外一个链表.
-    Link<int> list_2;;
-    list_2.insertHead(7);
-    list_2.insertHead(6);
-    list.connect(list_2);
-    list.print();
-    return 0;
-
-
-}
-void dataStructTest(){
-    // 题目1 顺序插入
-    Link<int> list;
-    list.insertHead(5);
-    list.insertHead(3);
-    list.insertHead(2);
-    list.insertHead(1);
-    cout<<"list: ";
-    list.print();
-    list.addByOrdered(4);
-    cout<< "after adding: ";
-    list.print();
-
-    //题目2 倒置
-    list.reverse();
-    cout<<"after reversing: ";
-    list.print();
-
-    //题目3 链接
-    Link<int> list_2;
-    list_2.insertHead(8);
-    list_2.insertHead(7);
-    list_2.insertHead(6);
-    cout<<"list_2: ";
-    list_2.print();
-    list.connect(list_2);
-    cout<<"list + list_2: ";
-    list.print();
-    list.sort();
-    cout<< "after sorting: ";
-    list.print();
-    list.reverse();
-    cout<< "after reversing: ";
-    list.print();
-}
-void test_1(){
-    Link<Empoyee> list;
-    Empoyee no1{ "no1",001 }, no2{ "no2",002 }, no3{"no3",003}, no4{ "no4",004 };
-
-    //插入功能测试.
-    cout << "Insert test:" << endl;
-    list.insertHead(no1);
-    list.insert(no2, 1);
-    list.insert(no3, 2);
-    list.insert(no4, 3);
-    list.print();
-
-    //测试反序功能.
-    cout << "reserve test:" << endl;
-    list.reverse();
-    cout << "reversed link is:" << endl;
-    list.print();
-
-    //测试删除功能.
-    cout << "remove test:" << endl;
-    cout << "the longth of link is:" << list.getLength()<<endl;
-    list.remove(no4);
-    list.print();
-    cout << "the length of link is:" << list.getLength()<< endl;
-    list.remove(no4);
-    list.print();
-
-    //List类测试
+    list.insert(3,4);
+    cout<<list<<endl;
 }
